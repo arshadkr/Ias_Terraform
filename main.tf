8ad6609bef3701c5382586b3e6fbb8ae45b9689e
arshadkr <arshadkr@gmail.com>
2023-05-28 17:15:24 +0900

    added sql firewall for local ip

--- main.tf
+++ main.tf
@@ -38,3 +38,14 @@ resource "azurerm_sql_database" "appdb" {
     azurerm_sql_server.app_server
   ]
 }
+
+resource "azurerm_sql_firewall_rule" "app_server_firewall_rule" {
+  name                = "app_server_firewall_rule"
+  resource_group_name = azurerm_resource_group.app_grp.name
+  server_name         = azurerm_sql_server.app_server.name
+  start_ip_address    = "113.154.47.146"
+  end_ip_address      = "113.154.47.146"
+  depends_on = [
+    azurerm_sql_server.app_server
+  ]
+}
