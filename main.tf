48b5a70b59c86b61dd9f3a2d106a623736c9c890
arshadkr <arshadkr@gmail.com>
2023-05-29 21:17:37 +0900

    formated variables

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_resource_group" "apps_grp" {
+resource "azurerm_resource_group" "apps-grp" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_sql_server" "app_server" {
+resource "azurerm_sql_server" "app-server" {
   name                         = var.app_server001
-  resource_group_name          = azurerm_resource_group.apps_grp.name
-  location                     = azurerm_resource_group.apps_grp.location
+  resource_group_name          = azurerm_resource_group.apps-grp.name
+  location                     = var.location
   version                      = var.sql_version
   administrator_login          = var.sql_uname
   administrator_login_password = var.sql_passwd
@@ -15,21 +15,21 @@ resource "azurerm_sql_server" "app_server" {
 
 resource "azurerm_sql_database" "appdb" {
   name                = var.app_db
-  resource_group_name = azurerm_resource_group.apps_grp.name
-  location            = azurerm_resource_group.apps_grp.location
-  server_name         = azurerm_sql_server.app_server.name
+  resource_group_name = azurerm_resource_group.apps-grp.name
+  location            = var.location
+  server_name         = azurerm_sql_server.app-server.name
   depends_on = [
-    azurerm_sql_server.app_server
+    azurerm_sql_server.app-server
   ]
 }
 
 resource "azurerm_sql_firewall_rule" "app_server_firewall_rule" {
   name                = var.app_server_firewall_rule
-  resource_group_name = azurerm_resource_group.apps_grp.name
-  server_name         = azurerm_sql_server.app_server.name
+  resource_group_name = azurerm_resource_group.apps-grp.name
+  server_name         = azurerm_sql_server.app-server.name
   start_ip_address    = "113.154.47.146"
   end_ip_address      = "113.154.47.146"
   depends_on = [
-    azurerm_sql_server.app_server
+    azurerm_sql_server.app-server
   ]
 }
