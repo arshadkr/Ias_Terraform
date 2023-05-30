6992cc65c61b9586656875e7dfde40d98636b6bc
arshadkr <arshadkr@gmail.com>
2023-05-30 12:44:47 +0900

    removed variable in backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.azurerm_resource_group
-    storage_account_name = var.storage_name
-    container_name       = var.storage_container_name
+    resource_group_name  = "apps_grp"
+    storage_account_name = "jrcsstorage"
+    container_name       = "tfstatecontainer"
     key                  = "terraform.tfstate"
   }
 }
