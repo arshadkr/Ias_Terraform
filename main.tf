42a8b63801975a0ef354a24539ca871c55df23be
arshadkr <arshadkr@gmail.com>
2023-06-03 21:36:42 +0900

    fixed typo error

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_key_vault" "app_vault" {
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
   sku_name                   = "standard"
-  access_policy = {
+  access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
     key_permission = [
