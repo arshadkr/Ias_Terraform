78d3687f26b65a5d8007ab50b0521f7e2865b21a
arshadkr <arshadkr@gmail.com>
2023-09-01 19:34:12 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "apps_grp"
-    storage_account_name = "jrcsstorage"
+    storage_account_name = "tfstorage"
     container_name       = "tfstatecontainer"
     key                  = "terraform.tfstate"
   }
