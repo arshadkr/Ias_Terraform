0cdc3c20dfe4fb9793eeac89ca2de0ab1a1fef0c
arshadkr <arshadkr@gmail.com>
2023-09-01 19:32:25 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ resource "azurerm_sql_firewall_rule" "app_server_firewall_rule" {
   name                = var.app_server_firewall_rule
   resource_group_name = azurerm_resource_group.apps-grp.name
   server_name         = azurerm_sql_server.app-server.name
-  start_ip_address    = "113.154.47.146"
-  end_ip_address      = "113.154.47.146"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
   depends_on = [
     azurerm_sql_server.app-server
   ]
